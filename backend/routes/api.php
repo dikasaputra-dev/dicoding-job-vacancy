350224390e2b174dba90b20e98b245f770c7094c
dikasaputra-dev <tdikasaputraz@gmail.com>
2026-07-27 16:28:29 +0700

    feat: implement vacancy detail endpoint

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,21 @@
 <?php
 
 use App\Http\Controllers\Api\V1\VacancyController;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')
     ->name('api.v1.')
     ->group(function (): void {
-        Route::apiResource('vacancies', VacancyController::class);
+        Route::apiResource(
+            'vacancies',
+            VacancyController::class)
+            ->missing(
+                static function (Request $_request): JsonResponse {
+                    return response()->json([
+                        'message' => 'Vacancy not found.',
+                    ], 404);
+                }
+            );
     });
